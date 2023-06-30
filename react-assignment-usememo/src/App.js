7e8695ed0cc033def006b05a5e983f1b6900fb5d
ajaygajulaeverest <ajay.gajula@everest.engineering>
2023-06-30 10:32:28 +0530

    usememo was used to create a todo list of completed items and incompleyed items

--- react-assignment-usememo/src/App.js
+++ react-assignment-usememo/src/App.js
@@ -1,36 +1,73 @@
+import React, { useState, useEffect, useMemo } from "react";
+// import axios from 'axios';
 import "./App.css";
-import React from "react";
+import Buttons from "./buttons";
 
+function Completedlist() {
+  return completedItems.map((user) => {
+    return (
+      <p className="completeditem">
+        {user.id} - Title: {user.title} completed
+      </p>
+    );
+  });
+}
+function Incompletedlist() {
+  return incompletedItems.map((user) => {
+    return (
+      <p className="incompleteditem">
+        {user.id} - Title: {user.title}
+      </p>
+    );
+  });
+}
+let completedItems = [];
+let incompletedItems = [];
 function App() {
-  const [todo, setTodo] = React.useState([{ additem: "Add Todo items" }]);
-  const [text, setText] = React.useState("");
-  const handleText = (e) => {
-    setText(e.target.value);
+  const [users, setUsers] = useState([]);
+  const [completeditems, setCompletedItems] = useState(false);
+  const [incompleteditems, setIncompletedItems] = useState(false);
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    setUsers(data);
+    // console.log(data);
   };
-  const handleAddTodo = () => {
-    setTodo(todo.concat({ additem: text }));
-    setText("");
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  completedItems = useMemo(
+    () => users.filter((item) => item.completed === true),
+    [users]
+  );
+  incompletedItems = useMemo(
+    () => users.filter((item) => item.completed === false),
+    [users]
+  );
+ 
   return (
-    <div className="main">
-      <input type="text" value={text} onChange={handleText} />
-      <button type="button" onClick={handleAddTodo}>
-        Add todo
-      </button>
-      <Todo list={todo} />
+    <div className="maindiv">
+      <h1>
+        <Buttons
+          Prop="completed items"
+          setCompleted={() => {
+            setCompletedItems(true);
+            setIncompletedItems(false);
+          }}
+        />
+        <Buttons
+          Prop="incompletd items"
+          setCompleted={() => {
+            setIncompletedItems(true);
+            setCompletedItems(false);
+          }}
+        />
+      </h1>
+      <>
+        {completeditems && <Completedlist />}
+        {incompleteditems && <Incompletedlist />}
+      </>
     </div>
   );
 }
-const Todo = React.memo(({ list }) => {
-  return (
-    <ul>
-      {list.map((item) => (
-        <TodoItem item={item} />
-      ))}
-    </ul>
-  );
-});
-const TodoItem = React.memo(({ item }) => {
-  return <li>{item.additem}</li>;
-});
 export default App;
--- react-assignment-usememo/src/App.test.js
+++ react-assignment-usememo/src/App.test.js
@@ -1,13 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Add Todo items");
+  const linkElement = screen.getByText("completed items");
   expect(linkElement).toBeInTheDocument();
 });
-test("renders learn react link", () => {
+
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Add todo");
+  const linkElement = screen.getByText("incompletd items");
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
